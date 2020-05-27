4e502ebee4f316a54f6aa0ca1491708e63462f86
danielemarinazzo <daniele.marinazzo@gmail.com>
2020-05-27 12:27:18 +0200

    added AEC

--- test_psd_AEC.m
+++ test_psd_AEC.m
@@ -3,10 +3,10 @@ T = 1; % Length of timeseries in seconds
 fs = 256; % Sample rate of timeseries
 ns=round(T*fs);
 nt=10; %trials
-f0 = [6 10]; % theta frequencies
+f0 = [2 6 10]; % delta, theta, alpha frequencies
 df = 1/T;
 data=zeros(nc,ns);
-PRtot=linspace(1,4,100); % ratio of power of second frequency wrt first one
+PRtot=linspace(1,4,100); % ratio of power of last frequency wrt first two
 AEC_meas=zeros(nc,nc,length(PRtot));AEC_no_meas=AEC_meas;
 AEC_av=zeros(1,length(PRtot));AEC_no_av=AEC_av;
 iband=[8 12];
@@ -16,7 +16,7 @@ win5cyc=5*cycpersec;
 filtorder=round(fs*win5cyc);
 filtPts = fir1(filtorder, 2/fs*iband);
 for ip=1:length(PRtot)
-    rmsLevel = [1;1/sqrt(PRtot(ip))]; % RMS level
+    rmsLevel = [1;1;1/sqrt(PRtot(ip))]; % RMS level
     GxxMag = ((rmsLevel.^2)/df); % Single-Sided Power spectrum magnitude
     SxxMag = GxxMag/2; % Double-sided Power spectrum magnitude
     PSD_DoubleSided = zeros(T*fs, 1);
@@ -35,7 +35,9 @@ for ip=1:length(PRtot)
     AEC_no_av(ip)=nanmean(nanmean(nanmean(AEC_no_meas)));
 end
 %%
-figure;scatter(1./PRtot,AEC_av);xlabel('PSD(theta/alpha ratio)');ylabel('|AEC alpha|');set(gca,'FontSize',14)
-title(['Pearson correlation = ' num2str(corr2(PRtot,AEC_av))])
-figure;scatter(1./PRtot,AEC_no_av);xlabel('PSD(theta/alpha ratio)');ylabel('|AEC alpha no orth|');set(gca,'FontSize',14)
-title(['Pearson correlation = ' num2str(corr2(PRtot,AEC_no_av))])
+figure;scatter(1./PRtot,AEC_av);xlabel('PSD((theta+delta)/alpha ratio)');ylabel('|AEC alpha|');set(gca,'FontSize',14)
+c=corr((1./PRtot)',AEC_av','Type','Spearman');
+title(['Spearman correlation = ' num2str(c)]);
+figure;scatter(1./PRtot,AEC_no_av);xlabel('PSD((theta+delta)/alpha ratio)');ylabel('|AEC alpha no orth|');set(gca,'FontSize',14)
+c=corr((1./PRtot)',AEC_no_av','Type','Spearman');
+title(['Spearman correlation = ' num2str(c)])
