094276d404264f783e2338e47077613a69bd9837
danielemarinazzo <daniele.marinazzo@gmail.com>
2020-05-27 11:18:16 +0200

    added AEC

--- test_psd_AEC.m
+++ test_psd_AEC.m
@@ -3,12 +3,18 @@ T = 1; % Length of timeseries in seconds
 fs = 256; % Sample rate of timeseries
 ns=round(T*fs);
 nt=10; %trials
-f0 = [20;6]; % two frequencies
+f0 = [6 10]; % theta frequencies
 df = 1/T;
 data=zeros(nc,ns);
-PRtot=linspace(1,2,100); % ratio of power of second frequency wrt first one
+PRtot=linspace(1,4,100); % ratio of power of second frequency wrt first one
 AEC_meas=zeros(nc,nc,length(PRtot));AEC_no_meas=AEC_meas;
 AEC_av=zeros(1,length(PRtot));AEC_no_av=AEC_av;
+iband=[8 12];
+avband=mean(iband);
+cycpersec=1/avband;
+win5cyc=5*cycpersec;
+filtorder=round(fs*win5cyc);
+filtPts = fir1(filtorder, 2/fs*iband);
 for ip=1:length(PRtot)
     rmsLevel = [1;1/sqrt(PRtot(ip))]; % RMS level
     GxxMag = ((rmsLevel.^2)/df); % Single-Sided Power spectrum magnitude
@@ -19,16 +25,17 @@ for ip=1:length(PRtot)
     for itrials=1:nt
         for ic=1:nc
             [timeseries, time] = TimeseriesFromPSD(PSD_DoubleSided, fs, T, 0);
-            data(ic,:)=timeseries+.1*randn(size(timeseries));
+            data(ic,:)=timeseries+.001*randn(size(timeseries));
         end
-        AEC_meas(:,:,itrials)=AEC(data');
-        AEC_no_meas(:,:,itrials)=AEC_noorth(data');
+        filteredData = filter(filtPts, 1, data',[],1);
+        AEC_meas(:,:,itrials)=AEC(filteredData);
+        AEC_no_meas(:,:,itrials)=AEC_noorth(filteredData);
     end
     AEC_av(ip)=nanmean(nanmean(nanmean(AEC_meas)));
     AEC_no_av(ip)=nanmean(nanmean(nanmean(AEC_no_meas)));
 end
 %%
-subplot(1,2,1);scatter(PRtot,AEC_av);xlabel('PSD(theta/beta ratio)');ylabel('|AEC|');set(gca,'FontSize',14)
+figure;scatter(1./PRtot,AEC_av);xlabel('PSD(theta/alpha ratio)');ylabel('|AEC alpha|');set(gca,'FontSize',14)
 title(['Pearson correlation = ' num2str(corr2(PRtot,AEC_av))])
-subplot(1,2,2);scatter(PRtot,AEC_no_av);xlabel('PSD(theta/beta ratio)');ylabel('|AEC no orth|');set(gca,'FontSize',14)
+figure;scatter(1./PRtot,AEC_no_av);xlabel('PSD(theta/alpha ratio)');ylabel('|AEC alpha no orth|');set(gca,'FontSize',14)
 title(['Pearson correlation = ' num2str(corr2(PRtot,AEC_no_av))])
