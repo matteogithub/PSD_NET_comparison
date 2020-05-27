4ef501902aba2bf85bf97719749c3958a71cbfcd
danielemarinazzo <daniele.marinazzo@gmail.com>
2020-05-27 12:52:27 +0200

    added AEC

--- test_psd_AEC.m
+++ test_psd_AEC.m
@@ -6,7 +6,7 @@ nt=10; %trials
 f0 = [2 6 10]; % delta, theta, alpha frequencies
 df = 1/T;
 data=zeros(nc,ns);
-PRtot=linspace(1,4,100); % ratio of power of last frequency wrt first two
+PRtot=linspace(.25,1,100); % ratio of power of last frequency wrt first two
 AEC_meas=zeros(nc,nc,length(PRtot));AEC_no_meas=AEC_meas;
 AEC_av=zeros(1,length(PRtot));AEC_no_av=AEC_av;
 iband=[8 12];
